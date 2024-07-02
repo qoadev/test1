30cdf34d6523ff9643af8baec5564994cca0a095
Ruslan <salirr157@gmail.com>
2024-07-03 01:33:38 +0300

    fdg

---
+++ main.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int ar[100];
+    //int * const ptr_ar = ar;
+
+    int i=0;
+    while (scanf("%d", &*(ptr_ar+i)) && *(ptr_ar+i)!=78)
+    {
+        i++;
+    }
+
+    for (int j =0;j<i;++j)
+    {
+        printf("%d ", *(ptr_ar+j));
+    }
+
+    // здесь продолжайте программу
+
+    return 0;
+}
